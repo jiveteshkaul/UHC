de62db62ee9f9147cf649bf23909b89139369a0f
Jivetesh Kaul <jiveteshkaul@apples-MacBook-Pro-62.local>
2019-01-09 07:00:25 -0800

    Update

--- UHC_data_extractor.py
+++ UHC_data_extractor.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+import json
 
 def get_pdf(driver):
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="skip-to-main-content"]/div/div[3]/div[1]/a')))
@@ -13,12 +14,12 @@ def get_pdf(driver):
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/ul/li[2]/a/div[2]')))
     driver.find_element_by_xpath('//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/ul/li[2]/a/div[2]').click() #click email option
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/div/form/div[1]/div[1]/input')))
-    driver.find_element_by_xpath('//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/div/form/div[1]/div[1]/input').send_keys('jiveteshk@gmail.com',Keys.TAB,'komalm281095@gmail.com',Keys.TAB,'jkaul@apple.com',Keys.TAB,'',Keys.ENTER)
+    driver.find_element_by_xpath('//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/div/form/div[1]/div[1]/input').send_keys(mail_rec_1,Keys.TAB,mail_rec_2,Keys.TAB,mail_rec_3,Keys.TAB,mail_rec_4,Keys.ENTER)
     time.sleep(5)
 
 
 def navigate_to_page(state):
-    driver = webdriver.Chrome('/Users/jiveteshkaul/Downloads/chromedriver')
+    driver = webdriver.Chrome(chrome_driver_loc)
     driver.get('https://connect.werally.com/plans/allSavers/1') # SITE
     
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="step-1"]/div[2]/ul/li[2]/h2/div/button')))
@@ -46,13 +47,23 @@ def navigate_to_page(state):
     return(driver)
 
 
-def main(argv):
-    global driver
-#Take state name
-    if (len(sys.argv) < 2):
-        raise Exception("State not Provided While Executing the Program...Use : python <python_file> '<state_name>' ")
+def main():
+    global state_from_config, mail_rec_1, mail_rec_2, mail_rec_3, mail_rec_4, chrome_driver_loc
+    filename="./UHC_config.json"
+    configFile = open(filename)
+    root = json.load(configFile)
+    configFile.close()
+    
+    for config in root:
+        state_from_config = config['state']
+        mail_rec_1=config['mail_receiver_1']
+        mail_rec_2=config['mail_receiver_2']
+        mail_rec_3=config['mail_receiver_3']
+        mail_rec_4=config['mail_receiver_4']
+        chrome_driver_loc=config['chrome_driver_location']
+    
 #capitalize first letter    
-    args=sys.argv[1]
+    args=state_from_config
     words=args.split(" ")
     state=""
     for item in words:
@@ -60,11 +71,12 @@ def main(argv):
              
     chrome_driver=navigate_to_page(state)
     get_pdf(chrome_driver)
+
     
 if __name__=="__main__":
-
+    
     try:
-        main(sys.argv)
+        main()
         print('PDFs Sent successfully via email')
     except Exception as e:
         print(traceback.format_exc())
