b646ff82e048b1eacfcbcfa34bef10b0e4d595c9
Jivetesh Kaul <jiveteshkaul@apples-MacBook-Pro-62.local>
2019-01-17 07:33:59 -0800

    Updated

--- tmp.py
+++ tmp.py
@@ -8,53 +8,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 import json
 
-
-def convert_text_to_csv(tmp_file,final_op_csv):
-    file = open(tmp_file, 'r')
-    f = open(final_op_csv, 'w')
-    for line in file:
-
-        if line.strip() == "":
-            f.write("\n")
-                  
-        else:
-            f.write(line.strip().replace(",","|")+'|')
-             
-    f.close()            
-    
-def data_extractor(driver,page_count):
-
-    tmp_file='/Users/jiveteshkaul/personal_git_projs/tmp_data_file.txt' #Temporary file path
-    f = open(tmp_file, "w")
-    
-    #FOR 1st Page
-    cont1=driver.find_element_by_class_name('resultsWrapper')
+def get_pdf(driver):
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="skip-to-main-content"]/div/div[3]/div[1]/a')))
+    driver.find_element_by_xpath('//*[@id="skip-to-main-content"]/div/div[3]/div[1]/a').click() # click print/email option
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/ul/li[2]/a/div[2]')))
+    driver.find_element_by_xpath('//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/ul/li[2]/a/div[2]').click() #click email option
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/div/form/div[1]/div[1]/input')))
+    driver.find_element_by_xpath('//*[@id="ngdialog2"]/div[2]/div/div/div[2]/div/div/div/form/div[1]/div[1]/input').send_keys(mail_rec_1,Keys.TAB,mail_rec_2,Keys.TAB,mail_rec_3,Keys.TAB,mail_rec_4,Keys.ENTER)
     time.sleep(5)
-    container=cont1.find_elements_by_class_name('outer-container')
-    for item in container:
-        f.write(item.text) # Write data to file
-        f.write("\n")    # Separate by new line
-    f.close()
-    
-    #For remaining pages
-    count=1
-    next_button_xpath='//*[@id="skip-to-main-content"]/div/div[3]/div[2]/div/button[2]'
-    f = open(tmp_file, "a")
-    
-    while(count < page_count):
-        driver.find_element_by_xpath(next_button_xpath).click()
-        time.sleep(5)
-        cont1=driver.find_element_by_class_name('resultsWrapper')
-        time.sleep(5)
-        container=cont1.find_elements_by_class_name('outer-container')
-        for item in container:
-            f.write(item.text) # Write data to file
-            f.write("\n")   # Separate by new line
-        count+=1
-    f.close()
-    
-    convert_text_to_csv(tmp_file,final_op_csv)
-    
+
+
 def navigate_to_page(state):
     driver = webdriver.Chrome(chrome_driver_loc)
     driver.get('https://connect.werally.com/plans/allSavers/1') # SITE
@@ -78,18 +41,13 @@ def navigate_to_page(state):
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="step-1"]/div[2]/div[1]/ul/li[1]/guided-search-link/button/div[1]/img')))
     driver.find_element_by_xpath('//*[@id="step-1"]/div[2]/div[1]/ul/li[1]/guided-search-link/button/div[1]/img').click() # click primary care
     
-    time.sleep(5)
     WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="step-6"]/div[2]/div[1]/ul/li[1]/h2/div/guided-search-link/button')))
     driver.find_element_by_xpath('//*[@id="step-6"]/div[2]/div[1]/ul/li[1]/h2/div/guided-search-link/button').click() # click all primary care physi
-    
-    time.sleep(5)
-    pg_txt=driver.find_element_by_class_name('pagCopy').text #get total number of pages
-    page_count=int(pg_txt.split(" ")[-1])
-    
-    data_extractor(driver,page_count)
+    return(driver)
+
 
 def main():
-    global state_from_config, final_op_csv, chrome_driver_loc
+    global state_from_config, mail_rec_1, mail_rec_2, mail_rec_3, mail_rec_4, chrome_driver_loc
     filename="./UHC_config.json"
     configFile = open(filename)
     root = json.load(configFile)
@@ -97,10 +55,13 @@ def main():
     
     for config in root:
         state_from_config = config['state']
-        final_op_csv=config['csv_op_file']
+        mail_rec_1=config['mail_receiver_1']
+        mail_rec_2=config['mail_receiver_2']
+        mail_rec_3=config['mail_receiver_3']
+        mail_rec_4=config['mail_receiver_4']
         chrome_driver_loc=config['chrome_driver_location']
     
-#convert state to crrect format   
+#capitalize first letter    
     args=state_from_config
     words=args.split(" ")
     state=""
@@ -108,13 +69,17 @@ def main():
         state=state+item.capitalize()+" "
              
     chrome_driver=navigate_to_page(state)
+    get_pdf(chrome_driver)
+
     
 if __name__=="__main__":
     
     try:
         main()
-        print('Done !!')
+        print('PDFs Sent successfully via email')
     except Exception as e:
         print(traceback.format_exc())
         print("Error: {0}".format(e))
+
+
 
